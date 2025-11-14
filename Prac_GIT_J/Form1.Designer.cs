2414af4b61c35651dce82911b8bc5521585edacb
ElMauri19 <mauricioalejandrob16@gmail.com>
2025-11-13 22:04:32 -0500

    Add labels and buttons to Form

--- Prac_GIT_J/Form1.Designer.cs
+++ Prac_GIT_J/Form1.Designer.cs
@@ -28,13 +28,183 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.homero = new System.Windows.Forms.Label();
+            this.marge = new System.Windows.Forms.Label();
+            this.bart = new System.Windows.Forms.Label();
+            this.lisa = new System.Windows.Forms.Label();
+            this.maggie = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // homero
+            // 
+            this.homero.AutoSize = true;
+            this.homero.Location = new System.Drawing.Point(12, 323);
+            this.homero.Name = "homero";
+            this.homero.Size = new System.Drawing.Size(54, 16);
+            this.homero.TabIndex = 0;
+            this.homero.Text = "PADRE";
+            // 
+            // marge
+            // 
+            this.marge.AutoSize = true;
+            this.marge.Location = new System.Drawing.Point(304, 323);
+            this.marge.Name = "marge";
+            this.marge.Size = new System.Drawing.Size(56, 16);
+            this.marge.TabIndex = 1;
+            this.marge.Text = "MADRE";
+            // 
+            // bart
+            // 
+            this.bart.AutoSize = true;
+            this.bart.Location = new System.Drawing.Point(555, 323);
+            this.bart.Name = "bart";
+            this.bart.Size = new System.Drawing.Size(37, 16);
+            this.bart.TabIndex = 2;
+            this.bart.Text = "HIJO";
+            // 
+            // lisa
+            // 
+            this.lisa.AutoSize = true;
+            this.lisa.Location = new System.Drawing.Point(785, 323);
+            this.lisa.Name = "lisa";
+            this.lisa.Size = new System.Drawing.Size(36, 16);
+            this.lisa.TabIndex = 3;
+            this.lisa.Text = "HIJA";
+            // 
+            // maggie
+            // 
+            this.maggie.AutoSize = true;
+            this.maggie.Location = new System.Drawing.Point(1070, 323);
+            this.maggie.Name = "maggie";
+            this.maggie.Size = new System.Drawing.Size(89, 16);
+            this.maggie.TabIndex = 4;
+            this.maggie.Text = "HIJA MENOR";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(534, 440);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 43);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "CAMBIAR DE ROPA 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::Los_Simpson.Properties.Resources.maggie;
+            this.pictureBox5.Location = new System.Drawing.Point(1073, 0);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(182, 298);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 10;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Los_Simpson.Properties.Resources.lisa;
+            this.pictureBox4.Location = new System.Drawing.Point(771, 0);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(187, 298);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 9;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Los_Simpson.Properties.Resources.bart_normal;
+            this.pictureBox3.Location = new System.Drawing.Point(534, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(178, 298);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Los_Simpson.Properties.Resources.marge_normal;
+            this.pictureBox2.Location = new System.Drawing.Point(273, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(203, 298);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Los_Simpson.Properties.Resources.homero_normal1;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(208, 298);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(771, 440);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(187, 43);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "CAMBIAR DE ROPA 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1359, 495);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.maggie);
+            this.Controls.Add(this.lisa);
+            this.Controls.Add(this.bart);
+            this.Controls.Add(this.marge);
+            this.Controls.Add(this.homero);
+            this.Name = "Form1";
+            this.Text = "p";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label homero;
+        private System.Windows.Forms.Label marge;
+        private System.Windows.Forms.Label bart;
+        private System.Windows.Forms.Label lisa;
+        private System.Windows.Forms.Label maggie;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Button button2;
     }
 }
 
